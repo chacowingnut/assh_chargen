b8a5aba562cf82412abc709885374cc30836761b
Martian Tapir <nathaninkley@gmail.com>
2017-10-29 13:17:57 -0500

    Added class selection and populated most fighter class features

--- char_gen.py
+++ char_gen.py
@@ -12,6 +12,45 @@ TODO:
 
 from dice_things import AbiRoller
 
+fighter = {
+    'HD': 'd10',
+    'saves': 'Death +2, Transformation +2',
+    'FA': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    'XP': [0, 2e3, 4e3, 8e3, 16e3, 32e3, 64e3,
+           128e3, 256e3, 384e3, 512e3, 640e3],
+    'features': {
+        'Attack Rate': '1/1 from 1st to 6th level, 3/2 from 7th to 12th',
+        'Heroic Fighting': 'From 1st to 6th, double attacks per round against' +
+                           'enemies with less than 2 HD. After 7th, applies' +
+                           'for enemies with less than 3 HD.',
+        'Weapon Mastery': '',
+        'Gand Mastery': '',
+    }
+}
+
+mage = {}
+cleric = {}
+thief = {}
+
+classes = {'fighter': fighter, 'Mage': mage, 'cleric': cleric, 'thief': thief}
+
+
+def AvailableClasses(abilities):
+    '''Print the available classes given the indicated ability scores'''
+    available = ['Fighter', 'Mage', 'Cleric', 'Thief']
+    if abilities[0] < 10:
+        available.remove('Fighter')
+    if abilities[3] < 10:
+        available.remove('Mage')
+    if abilities[4] < 10:
+        available.remove('Cleric')
+    if abilities[1] < 10:
+        available.remove('Thief')
+
+    print '\nYou are capable of becoming:'
+    print available
+    return [k.lower() for k in available]
+
 
 def AbiPrinter(abilities):
     labels = [
@@ -47,6 +86,17 @@ if __name__ == '__main__':
     ability_generation = True
     while ability_generation:
         abilities = AbiDialog()
-        cool = raw_input('Are these to your liking? (y/n) >')
+        cool = raw_input('Are these to your liking? (y/n) \n>')
         if cool.lower() == 'y':
             ability_generation = False
+
+    available = AvailableClasses(abilities)
+    choosing_class = True
+    while choosing_class:
+        cla = raw_input('\nWhich do you choose?\n>')
+        if cla.lower() not in available:
+            print 'I am sorry, try again.'
+        else:
+            print 'Excellent choice, '+cla+'!'
+            choosing_class = False
+    print classes[cla]
